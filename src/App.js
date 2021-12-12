63038836fbd66f8c1ab40f4837409bc2bda2db73
jaasmaz <jaasmaz@gmail.com>
2021-12-11 21:59:05 -0800

    Google oAuth set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Button } from "@mui/material";
+import GoogleLogin from "react-google-login";
 import "./App.css";
+
+let responseGoogle = (res) => {
+  console.log(res);
+  console.log(res.profileObj);
+};
 
 function App() {
   return (
     <div className="App">
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy="single_host_origin"
+      ></GoogleLogin>
       <Button variant="outlined" size="large">
         Click me
       </Button>
