db629805d0cbd0fe3ca8096e72443963d7c6617a
jaasmaz <jaasmaz@gmail.com>
2021-12-11 21:39:46 -0800

    Initial commit with Material UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="outlined" size="large">
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
